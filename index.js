989ae7b573c0a82cb1ea59e9af15c0335b224339
reboot-ua <reboot.if.ua@gmail.com>
2019-03-16 21:49:15 +0200

    grid

---
+++ index.js
@@ -0,0 +1,31 @@
+
+document.getElementById('item1').onclick =function () {
+    document.getElementById('item1').style.backgroundColor = 'green';
+};
+document.getElementById('item2').onclick =function () {
+    document.getElementById('item2').style.backgroundColor = 'red';
+};
+document.getElementById('item3').onclick =function () {
+    document.getElementById('item3').style.backgroundColor = 'green';
+};
+document.getElementById('item4').onclick =function () {
+    document.getElementById('item4').style.backgroundColor = 'red';
+};
+document.getElementById('item5').onclick =function () {
+    document.getElementById('item5').style.backgroundColor = 'green';
+};
+document.getElementById('item6').onclick =function () {
+    document.getElementById('item6').style.backgroundColor = 'red';
+};
+document.getElementById('item7').onclick =function () {
+    document.getElementById('item7').style.backgroundColor = 'green';
+};
+document.getElementById('item8').onclick =function () {
+    document.getElementById('item8').style.backgroundColor = 'red';
+};
+document.getElementById('item9').onclick =function () {
+    document.getElementById('item9').style.backgroundColor = 'green';
+};
+document.getElementById('item10').onclick =function () {
+    document.getElementById('item10').style.backgroundColor = 'green';
+};
